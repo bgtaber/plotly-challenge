5ecfd3f39a56afc60d03b7a5cca7f11e046b83a6
Bailey Taber <bailey@Baileys-MacBook-Pro.local>
2021-04-03 12:49:37 -0600

    .slice?

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,68 +1,82 @@
-//dropdown 
-
-// function updatePage(){
-// 	var dropdown=d3.selectAll('#selDataset');
-// 	d3.json('samples.json').then((samples)=>{
-// 	samples['names'].forEach((name)=>{
-// 	dropdown.append('option').text(name).property('value', name);
-// 	});
-// 	});
+//Step 2: 
+// Use D3 to create an event handler
+// function updatePage() {
+//   d3.json('samples.json').then((data)=> {
+//     var dropdownMenu = d3.select("#selDataset"); //# means id // . means class
+//       data['names'].forEach((id_num)=> {
+//         dropdownMenu.append('option').text(id_num).property('value', id_num);	//.text method grab (name text)
+//     });
+//   });
 // }
-
-//////NO
-// 	var dropdownNames=dropdown.id;
-// 	var selectedOption=dropdown.value;
-
-// 	console.log(dropdownNames);
-// 	console.log(selectedOption);
-// };
-///END NO
 
 // updatePage();
-	// NO d3.json("samples.json").then((samples)=>{
 
-
-// }
-// //use d3 to read in samples.json
-function buildPlot(otu_id){
-
-d3.json("samples.json").then((data)=>{
-	//
-	var sample = data['samples'].filter(object=>object.id)[0];
-	var otu_top = (sample['otu_ids'].slice(0,10)).reverse();
-
- 
-	console.log(sample);
-	console.log(otu_top);
-});
+function updatePage(){
+d3.json('././samples.json').then((data)=> {
+	var dropdown =d3.select('#selDataset');
+	data['names'].forEach((nameid)=>{
+		dropdown.append('option').text(nameid).property('value', nameid)
+	});
+ });
 }
 
-///IDK yeT
-// function buildPlot() {
-// 	d3.json('samples.json').then((samples)=>{
-// 	console.log(samples)
+updatePage();
 
-// 	var otu_ids = samples.samples.map(object => object.otu_ids)
+function buildPlot(name_id){
+ d3.json('././samples.json').then((data)=>{
+  var samples = data.samples.filter(d=>d.id.toString()===name_id)[0];
+  var metadata = data.metadata.filter(d=>d.id.toString()===name_id)[0];
+  var otu_ids = samples.otu_ids;
+  var samplevalues = samples.samples_values;
+  var hover_text=samples.otu_labels;
+  var otu_label= otu_ids.map(x=> `otu ${x}`).slice(0,10).reverse()
 
-// });
+  var trace_bar= {
+      'type': 'bar',
+      'y': otu_label,
+      'x': samplevalues.slice(0,10).reverse(),
+      'text': hover_text.slice(0,10).reverse(),
+      'orientation': 'h'
+    };
+ 
 
-// var data = ['samples.json']
+ var bar_layout={
+   'title': 'Top 10 Clusters Found',
+   't': 30,
+   'b': 30
+ };
 
-// function buildPlot(){
-// 	d3.json(data).then(function(sample){
-// 		var names = samples['names'];
-// 		var metadata = samples['metadata'];
-// 		var samples = samples['samples'];
+ Plotly.newPlot('bar', [trace_bar], [bar_layout]); //potential change square brackets
 
-// 	getSample();
-	
-// 	var layout = {
-// 		type: 'bar'
-// 	};
+   var trace_bubble= {
+     'type': 'scatter',
+     'y': otu_ids,
+     'x': samplevalues,
+     mode: 'markers',
+     text: hover_text,
+     marker: {
+     size: samplevalues,
+        color: otu_ids
+      }
+    };
 
-// 	Plotly.newPlot('bar', data, layout)
+    var bubble_layout= {
+      xaxis: {title:{text: 'OTU ID'}}
+    };
 
-// 	});
-// }
 
-// buildPlot();
+      Plotly.newPlot('bubble', [trace_bubble], [bubble_layout]);
+
+    var tbody=d3.select('tbody');
+    tbody.html('')
+    
+    Object.entries(metadata).forEach(([key,value])=>{
+      var row=tbody.append("tr");
+      row.text(`${key}: ${value}`);
+ 
+    });
+
+  });
+}
+
+
